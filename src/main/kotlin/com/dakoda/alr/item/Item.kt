7b6c5605ea7a77cd747817f16d38c0f68a6559a6
dakodabutnot <southforwinter6@gmail.com>
2019-10-24 21:20:09 +0100

    Included extra Item fields and reflected changes in tests

--- src/main/kotlin/com/dakoda/alr/item/Item.kt
+++ src/main/kotlin/com/dakoda/alr/item/Item.kt
@@ -1,15 +1,20 @@
 package com.dakoda.alr.item
 
 import org.valiktor.functions.isNotBlank
+import org.valiktor.functions.isPositiveOrZero
 import org.valiktor.validate
 
 data class Item(
     val name: String,
-    val type: ItemType
+    val type: ItemType,
+    val description: String = "",
+    val value: Int = 0
 ) {
+
     init {
         validate(this) {
             validate(Item::name).isNotBlank()
+            validate(Item::value).isPositiveOrZero()
         }
     }
 }
--- src/test/kotlin/com/dakoda/alr/item/ItemTest.kt
+++ src/test/kotlin/com/dakoda/alr/item/ItemTest.kt
@@ -10,18 +10,39 @@ class ItemTest {
     @Test
     fun `When creating an item with a blank name, then throw a Valiktor constraint violation exception`() {
         assertThrows<ConstraintViolationException> {
-            Item("", ItemType.WEAPON)
+            Item(
+                name = "",
+                type = ItemType.WEAPON
+            )
+        }
+    }
+
+    @Test
+    fun `When creating an item with a negative value, then throw a Valiktor constraint violation exception`() {
+        assertThrows<ConstraintViolationException> {
+            Item(
+                name = "test item",
+                type = ItemType.WEAPON,
+                value = -1
+            )
         }
     }
 
     @Test
     fun `When creating an item, it is instantiated correctly`() {
-        val item: Item? = Item("test item", ItemType.WEAPON)
+        val item: Item? = Item(
+            name = "test item",
+            type = ItemType.WEAPON,
+            description = "a weapon",
+            value = 0
+        )
 
         dassert {
             item.isNotNull
             item?.name equals "test item"
             item?.type equals ItemType.WEAPON
+            item?.description equals "a weapon"
+            item?.value equals 0
         }
     }
 }
